63b17add4d198fc980da9171a0e067980a9207ff
Richard Case <Richard Case>
2016-08-19 21:57:17 +0100

    Started to add Go worker. Not working currently

---
+++ code/src/hasher/hasher.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Hapi = require('hapi');
+const crypto = require('crypto');
+
+const server = new Hapi.Server();
+
+server.connection({ port: 8001 });
+
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: function (request, reply) {
+        // sleep 0.1
+        setTimeout(function() {
+            var body = request.payload;
+            var hash = crypto.createHash('sha256').update(body).digest("hex");
+            reply(hash);
+        }, 100);
+        
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hasher running on ' + server.info.uri);
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Server running at:' + server.info.uri);
+});
